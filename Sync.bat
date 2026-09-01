4fad189e4d31b2b5d4d4fb8e97315172721babad
teacher-website <teacher@csps.tp.edu.tw>
2026-09-01 09:06:49 +0800

    update 2026/09/01 週二  9:06:49.16

--- Sync.bat
+++ Sync.bat
@@ -3,5 +3,5 @@ cd /d "%~dp0"
 powershell -ExecutionPolicy Bypass -File "%~dp0generate_index.ps1"
 git add .
 git commit -m "update %date% %time%"
-git push -u origin main
+git push origin main --force
 pause
